2a733e1e8a45a137e63b7109688ed0a0cdfdd166
steve <nice_mini@naver.com>
2025-01-13 15:42:29 +0900

    기존 chat이름 aichat으로 변경

--- src/main/java/com/bbook/controller/AIChatController.java
+++ src/main/java/com/bbook/controller/AIChatController.java
@@ -2,7 +2,7 @@ package com.bbook.controller;
 
 import com.bbook.dto.ChatMessageRequestDto;
 import com.bbook.dto.ChatMessageResponseDto;
-import com.bbook.service.ChatService;
+import com.bbook.service.AIChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/ai-chat")
 @RequiredArgsConstructor
-public class ChatController {
+public class AIChatController {
 
-  private final ChatService chatService;
+  private final AIChatService aiChatService;
 
   @PostMapping("/message")
   public ResponseEntity<ChatMessageResponseDto> processMessage(@RequestBody ChatMessageRequestDto request) {
-    ChatMessageResponseDto response = chatService.processMessage(request);
+    ChatMessageResponseDto response = aiChatService.processMessage(request);
     return ResponseEntity.ok(response);
   }
 }
--- src/main/java/com/bbook/service/AIChatService.java
+++ src/main/java/com/bbook/service/AIChatService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ChatService {
+public class AIChatService {
 
   private final BookRepository bookRepository;
 
